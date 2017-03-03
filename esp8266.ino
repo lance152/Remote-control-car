84e5e319edafc1934492bb1a75f61787b4e52ff0
Runzhou Fan <runzhoufan@LABS.SCE.CARLETON.CA>
2017-03-03 17:05:54 -0500

    add wifi module code

---
+++ esp8266.ino
@@ -0,0 +1,44 @@
+#include <SoftwareSerial.h>
+#include <ESP8266wifi.h>
+
+WiFiClient client;
+const char *host = "";
+const int tcpPort = 000;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  Serial.println();
+
+  WiFi.begin("", "");// wifi network name and passwork
+  Serial.print("Connecting";
+  
+  while(Wifi.status()!=WL_CONNECTED))
+  {
+    delay(500);
+    Serial.print(".");
+    }
+    Serial.println();
+
+   Serial.print("Connected, IP address: ");
+   Serial.print(WiFi.localIP());
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+    while(!client.connected()) // not connectd to the server
+    { 
+      if(!client.connect(host, tcpPort))
+      {
+        Serial.print("Connecting to server...");
+        delay(500);
+        }
+      }
+
+     while(client.available())
+     {
+      char val = client.read();
+      Serial.println(val);
+      }
+}
